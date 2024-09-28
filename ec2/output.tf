b28f681f29be81cdfa0b0ff41bc61799ff2c5b89
rahul-awsdevops <rahulranjana12@gmail.com>
2024-09-28 23:26:13 +0530

    Update output.tf

--- ec2/output.tf
+++ ec2/output.tf
@@ -1,11 +1,11 @@
 output "instance_id" {
-  value = aws_instance.this.id
+  value = aws_instance.this[*].id
 }
 
 output "eip" {
-  value = var.create_eip ? aws_eip.this.public_ip : null
+  value = var.create_eip ? aws_eip.this[*].public_ip : null
 }
 
 output "ebs_volume_id" {
-  value = var.create_extra_ebs ? aws_ebs_volume.extra_volume.id : null
+  value = var.create_extra_ebs ? aws_ebs_volume.extra_volume[*].id : null
 }
